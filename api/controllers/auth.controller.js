9f70abd5a5d037d232207f5d138ce7585ed0449c
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-04-07 12:17:21 +0530

    added midlleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,11 +2,14 @@
 
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async(req , res) => {
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async(req , res ,next) => {
 
     const {username , email , password} = req.body;
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message : 'All fields are required'});
+      // return res.status(400).json({message : 'All fields are required'});
+     return next(errorHandler(400 , 'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password , 10);
@@ -21,7 +24,7 @@ export const signup = async(req , res) => {
     res.json("SignUp successful");
     }
     catch(err){
-        return res.status(500).json({message: err.message});
+        next(err);
     }
     return 1;
 }
