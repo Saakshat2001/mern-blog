3c69e671744b869e9a206fef120bf04980e2d855
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-04-20 08:33:14 +0530

    completer user image upload functionality

--- client/src/Components/DashProfile.jsx
+++ client/src/Components/DashProfile.jsx
@@ -1,20 +1,92 @@
-import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { getDownloadURL, getStorage, ref,uploadBytesResumable } from "firebase/storage";
+import {app} from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
+  const filePickerRef = useRef();
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const[imageFileUploadProgress , setimageFileUploadProgress] = useState(null);
+  const[imageFileUploadError , setimageFileUploadError] = useState(null);
+ console.log(imageFileUploadProgress , imageFileUploadError);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+
+    setimageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on('state_changed' ,(snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; 
+      setimageFileUploadProgress(progress.toFixed(0)); // storing image to firebase
+     
+
+    } , (error) => {
+            setimageFileUploadError('Could not upload image (File must be less than 2MB )');
+            setimageFileUploadProgress(null);
+            setImageFile(null);
+            setImageFileUrl(null);
+    },() => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setImageFileUrl(downloadURL);
+      })
+    })
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4 ">
-        <div className="w-32 h-32 self-center cursor-pointer ">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer"
+          onClick={() => filePickerRef.current.click()}
+        >
+          { imageFileUploadProgress && <CircularProgressbar value={imageFileUploadProgress || 0}
+          text={`${imageFileUploadProgress}%`} 
+          strokeWidth={5}
+          styles={{
+            root:{
+              width: '100%',
+              height: '100%',
+              position:'absolute',
+              top:0,
+              left:0
+            },
+            path:{ 
+              stroke:`rgba(61,152,99) ${imageFileUploadProgress/100}`
+            }
+          }}/> }
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]`}
           />
         </div>
+      {  imageFileUploadError && <Alert color='failure'>{imageFileUploadError}
+        </Alert> }
         <TextInput
           type="text"
           id="username"
