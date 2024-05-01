e9c9c1ab33f7b15ad7e3fa9be3a8b91be2524c99
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-05-02 04:36:06 +0530

    create getpost api route

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -77,7 +77,7 @@ export default function CreatePost() {
         <h1 className='text-center text-3xl my-7 font-semibold'> Create a Post </h1>
         <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                <TextInput type = 'text' placeholder='Title' requireed id = 'title' className='flex-1'
+                <TextInput type = 'text' placeholder='Title' required id = 'title' className='flex-1'
                 onChange={(e) => setFormData({...formData , title: e.target.value})} /> 
                <Select
                onChange={(e) => setFormData({...formData , category: e.target.value})}
