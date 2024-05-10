66c022f4f2d694dfd2a27ecf072c1e6107837744
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-05-10 10:38:09 +0530

    add delete functionality to the dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,11 @@
-import express from 'express';
+import express from "express";
 
-import { verifyToken } from '../utils/verifyUser.js'; 
-import {create} from '../controllers/post.controller.js' 
-import { getposts } from '../controllers/post.controller.js';
+import { verifyToken } from "../utils/verifyUser.js";
+import { create, deletepost } from "../controllers/post.controller.js";
+import { getposts } from "../controllers/post.controller.js";
 const router = express.Router();
 
-router.post('/create' , verifyToken , create);
-router.get('/getposts' , getposts);
-
+router.post("/create", verifyToken, create);
+router.get("/getposts", getposts);
+router.get("/deletepost/:postId/:userId", verifyToken, deletepost);
 export default router;
-
