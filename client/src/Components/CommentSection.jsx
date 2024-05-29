0548732832229524650b63b404dae47afedb7c6d
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-05-29 06:43:51 +0530

    add like functionality to the comment function

--- client/src/Components/CommentSection.jsx
+++ client/src/Components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Modal, TextInput, Textarea } from 'flowbite-react';
 import React, { useState , useEffect} from 'react'
 import { useSelector } from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link , useNavigate} from 'react-router-dom'
 import Comment from './Comment';
 
 
@@ -10,6 +10,7 @@ export default function CommentSection({postId}) {
      const [comment , setComment] = useState('');
      const [commentError, setCommentError] = useState(null); 
      const [comments , setComments] = useState([]);
+     const navigate = useNavigate();
      useEffect(() => {
         const getComments = async() => {
                         try{
@@ -22,11 +23,41 @@ export default function CommentSection({postId}) {
             }catch(error){
                 console.log(error.message);
             }
-            }
+            } 
             getComments();
 
      } , [postId])
     
+     const handleLike = async(commentId) => {
+        try{
+          if(!currentUser){
+           navigate('/sign-in');
+            return;
+          }
+          const res = await fetch(`/api/comment/likeComment/${commentId}`,
+            {
+              method:'PUT'
+            }
+          );
+    
+              if(res.ok){
+                const data = await res.json();
+                setComments(comments.map((comment) => 
+                  comment._id===commentId?{
+                    ...comment,
+                    likes:data.likes,
+                    numberOfLikes: data.likes.length
+                  }:comment
+                ) 
+              )
+              console.log(comments,' wohpo ')
+              }
+        }catch(error){
+          console.log(error);
+        }
+
+     }
+
      const handleSubmit = async(e) => {
             e.preventDefault();
             if(comment.length>200){
@@ -109,7 +140,7 @@ export default function CommentSection({postId}) {
             <Comment
               key={comment._id}
               comment={comment}
-            //   onLike={handleLike}
+               onLike={handleLike}
             //   onEdit={handleEdit}
             //   onDelete={(commentId) => {
             //     setShowModal(true);
