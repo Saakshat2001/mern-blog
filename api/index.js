ae8ab6ed868eecaff6225f9d85464928d53469e9
saakshat.tyagi@pentair.com <saakshat.tyagi@pentair.com>
2024-06-09 16:27:50 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,7 +16,7 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 });
 
-
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -27,7 +29,10 @@ app.use('/api/auth' , authRoutes);
 app.use('/api/post' , postRoutes);
 app.use('/api/comment' , commentRoutes);
 
-
+app.use(express.static(path.join(__dirname , '/client/dist')))
+app.get('*' , (req,res) => {
+    res.sendFile(path.join(__dirname , 'client' , 'dist' , 'index.html'));
+})
 app.use((err , req, res,next) => {       //middleware
     
         const statusCode = err.statusCode || 500;
